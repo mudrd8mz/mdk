0a6644158fc3dedb96b37f2a9a72393446caaa17
David Mudrák <david@moodle.com>
2021-01-08 09:03:38 +0100

    Stop guessing the export directory from the release number
    This stopped being reliable with the new version numbers like 3.11.

--- mdk/scripts/enfix-merge.sh
+++ mdk/scripts/enfix-merge.sh
@@ -5,10 +5,10 @@ set -e
 AMOSCLI="/home/mudrd8mz/www/html/langmoodleorg/local/amos/cli"
 ENFIXROOT="/home/mudrd8mz/tmp/export-enfix"
 DIRROOT=$(mdk info -v path)
-RELEASE=$(mdk info -v release)
+BRANCH=$(php -r 'define("MOODLE_INTERNAL", 1); @require("version.php"); echo $branch;')
 TMPDIR=tmp
 PHP=/usr/bin/php
 LOG=enfix.log
 
 cd ${DIRROOT}/
-${PHP} ${AMOSCLI}/enfix-merge.php --symlinksdir=${DIRROOT}/${TMPDIR} --enfixdir=${ENFIXROOT}/${RELEASE:0:3} | tee ${LOG}
+${PHP} ${AMOSCLI}/enfix-merge.php --symlinksdir=${DIRROOT}/${TMPDIR} --enfixdir=${ENFIXROOT}/${BRANCH} | tee ${LOG}
