cfbc4ba040c76b2a61db84970241bef5bc517769
David Mudrák <david@moodle.com>
2025-07-29 15:59:05 +0200

    MDLSITE-8135 Support version.php in a public folder location

--- mdk/scripts/enfix-merge.sh
+++ mdk/scripts/enfix-merge.sh
@@ -5,7 +5,12 @@ set -e
 AMOSCLI="/home/mudrd8mz/www/html/langmoodleorg/local/amos/cli"
 ENFIXROOT="/home/mudrd8mz/tmp/export-enfix"
 DIRROOT=$(mdk info -v path)
-BRANCH=$(sed -n "s/\$branch   = '\(.*\)';.*/\1/p" version.php)
+if [[ -f version.php ]]; then
+    VERSIONPHP=version.php
+else
+    VERSIONPHP=public/version.php
+fi
+BRANCH=$(sed -n "s/\$branch   = '\(.*\)';.*/\1/p" ${VERSIONPHP})
 TMPDIR=tmp
 PHP=/usr/bin/php
 LOG=enfix.log
